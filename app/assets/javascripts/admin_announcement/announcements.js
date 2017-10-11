5c1ea8c0b22dc31b29dc8d683faef3a155f07a8f
nitanshu <niverma@q3tech.com>
2017-10-11 15:00:24 +0530

    1.) Assets bootstrap added 2.) In application.css and application .js is now similar with gubbara 3.) In application.js added hide_notice_panel function 4.) In application controller added layout admin_announcement 5.) Removed Test folder 6.) Updated .gitignore

---
+++ app/assets/javascripts/admin_announcement/announcements.js
@@ -0,0 +1,5 @@
+function hide_notice_panel(){
+    if ($('.notices .notice_panel').length == 0) {
+        $('.notices').hide();
+    }
+}
--- app/controllers/admin_announcement/application_controller.rb
+++ app/controllers/admin_announcement/application_controller.rb
@@ -1,5 +1,6 @@
 module AdminAnnouncement
   class ApplicationController < ActionController::Base
+    layout 'layouts/admin_announcement/application'
     protect_from_forgery with: :exception
   end
 end
